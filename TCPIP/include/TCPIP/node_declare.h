d7751a935e9b3d0aed7f00b89ff47c546b163b94
Dong-Young-Kim <bigdykim@gmail.com>
2022-07-26 18:02:22 +0900

    first fin code

---
+++ TCPIP/include/TCPIP/node_declare.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <time.h>
+#include <ros/ros.h>
+#include <boost/format.hpp>
+#include <std_msgs/String.h>
+#include <sensor_msgs/NavSatFix.h>          //gps
+#include <std_msgs/Float32MultiArray.h>     //ins
+#include "TCPIP/object_msg_arr.h"           //fusion
+
+using namespace std;
+
+//object
+struct objInfo_struct {
+    string classes      = "none";
+    unsigned int idx    = 0;
+    float x             = 0;
+    float y             = 0;
+    float z             = 0;
+    float xMin          = 0;
+    float yMin          = 0;
+    float zMin          = 0;
+    float xMax          = 0;
+    float yMax          = 0;
+    float zMax          = 0;
+};
+
+//gps
+struct gps_msg_struct{
+    double gps_lat      = 0;
+    double gps_lon      = 0;
+    double gps_alt      = 0;
+};
+
+//ins
+struct ins_msg_struct {
+    float kalman_lat    = 0;
+    float kalman_lon    = 0;
+    float kalman_alt    = 0;
+    float ins_lat       = 0;          //slow dont use
+    float ins_lon       = 0;          //slow dont use
+    float ins_alt       = 0;          //slow dont use
+    float accel_x       = 0;          //body frame
+    float accel_y       = 0;          //body frame
+    float accel_z       = 0;          //body frame
+    float gyro_x        = 0;
+    float gyro_y        = 0;
+    float gyro_z        = 0;
+    float quat_x        = 0;
+    float quat_y        = 0;
+    float quat_z        = 0;
+    float quat_w        = 0;
+    float ned_n         = 0;            //ins code start = origin
+    float ned_e         = 0;            //ins code start = origin
+    float ned_d         = 0;            //ins code start = origin
+    float enu_e         = 0;            //ins code start = origin
+    float enu_n         = 0;            //ins code start = origin
+    float enu_u         = 0;            //ins code start = origin
+};
--- TCPIP/src/C_TCPIP.cpp
+++ TCPIP/src/C_TCPIP.cpp
@@ -1,31 +1,14 @@
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<stdio.h>
-#include<iostream>
-#include<cstring>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <utility>
-#include <algorithm>
-#include <string>
-#include <time.h>
-#include <ros/ros.h>
-#include <boost/format.hpp>
-#include <std_msgs/String.h>
+#include <TCPIP/node_declare.h>
 
-
-
-#include "TCPIP/object_msg_arr.h"  //include "패키지 명/메시지 파일 명.h"
-
-using namespace std;
-
-#define SERV_PORT 15234
 #define SERV_ADDR "192.168.1.70"
+#define SERV_PORT 15234
+#define TOTALPACKETSIZE 50
 
-float tmp[50] = {11,12,13,314,4124,123,432,14,213,41234,124,1234,324,12,341,4,1234,1,234,1234,14,1,423};
+float final_packet[TOTALPACKETSIZE] = {11,12,13,314,4124,123,432,14,213,41234,124,1234,324,12,341,4,1234,1,234,1234,14,1,423};
+
+objInfo_struct objInfo_msg;
+gps_msg_struct gps_msg;
+ins_msg_struct ins_msg;
 
 
 pair<int,int> handShake(){
@@ -57,23 +40,65 @@ pair<int,int> handShake(){
     return make_pair(serv_sock, clnt_sock);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//recive data and save
 void recv_fusion(const TCPIP::object_msg_arrConstPtr& fusn_arr){
     for (const TCPIP::object_msg& fusn_obj : fusn_arr->object_msg_arr){
-        
+        objInfo_msg.classes = fusn_obj.classes;
+        objInfo_msg.idx     = fusn_obj.idx;
+        objInfo_msg.x       = fusn_obj.x;
+        objInfo_msg.y       = fusn_obj.y;
+        objInfo_msg.z       = fusn_obj.z;
+        objInfo_msg.xMin    = fusn_obj.xMin;
+        objInfo_msg.yMin    = fusn_obj.yMin;
+        objInfo_msg.zMin    = fusn_obj.zMin;
+        objInfo_msg.xMax    = fusn_obj.xMax;
+        objInfo_msg.yMax    = fusn_obj.yMax;
+        objInfo_msg.zMax    = fusn_obj.zMax;
     }
+
 }
 
-void recv_gps(TCPIP::object_msg_arr gps_m){
+void recv_gps(const sensor_msgs::NavSatFixConstPtr& gps_m){
+    gps_msg.gps_lat = gps_m->latitude;
+    gps_msg.gps_lon = gps_m->longitude;
+    gps_msg.gps_alt = gps_m->altitude;
     
 }
 
+void recv_ins(const std_msgs::Float32MultiArrayConstPtr& ins_m){
+    ins_msg.kalman_lat = ins_m->data.at( 0);
+    ins_msg.kalman_lon = ins_m->data.at( 1);
+    ins_msg.kalman_alt = ins_m->data.at( 2);
+    ins_msg.ins_lat    = ins_m->data.at( 3);          //slow, dont use
+    ins_msg.ins_lon    = ins_m->data.at( 4);          //slow, dont use
+    ins_msg.ins_alt    = ins_m->data.at( 5);          //slow, dont use
+    ins_msg.accel_x    = ins_m->data.at( 6);          //body frame
+    ins_msg.accel_y    = ins_m->data.at( 7);          //body frame
+    ins_msg.accel_z    = ins_m->data.at( 8);          //body frame
+    ins_msg.gyro_x     = ins_m->data.at( 9);
+    ins_msg.gyro_y     = ins_m->data.at(10);
+    ins_msg.gyro_z     = ins_m->data.at(11);
+    ins_msg.quat_x     = ins_m->data.at(12);
+    ins_msg.quat_y     = ins_m->data.at(13);
+    ins_msg.quat_z     = ins_m->data.at(14);
+    ins_msg.quat_w     = ins_m->data.at(15);
+    ins_msg.ned_n      = ins_m->data.at(16);            //ins code start = origin
+    ins_msg.ned_e      = ins_m->data.at(17);            //ins code start = origin
+    ins_msg.ned_d      = ins_m->data.at(18);            //ins code start = origin
+    ins_msg.enu_e      = ins_m->data.at(19);            //ins code start = origin
+    ins_msg.enu_n      = ins_m->data.at(20);            //ins code start = origin
+    ins_msg.enu_u      = ins_m->data.at(21);            //ins code start = origin
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 void send(int clnt_sock){
-    //write(clnt_sock, "server message\n", 15);
-    //memset(tmp,1,sizeof(tmp));
-    cout << tmp[0] << endl;
-    for(int i = 0; i < 50; i++)
-        write(clnt_sock, &tmp[i], sizeof(float));
+    cout << final_packet[0] << endl;
+    for(int i = 0; i < TOTALPACKETSIZE; i++)
+        write(clnt_sock, &final_packet[i], sizeof(float));
 }
 
 void recv(int clnt_sock){
@@ -89,18 +114,24 @@ int main(int argc, char* argv[]){
     ros::init(argc, argv, "TCPIP");             //node name 
 	ros::NodeHandle nh;                         //nodehandle
 
-    ros::Subscriber sub_fusion = nh.subscribe<TCPIP::object_msg_arr> ("/fusion_obj", 100, recv_fusion);
-    ros::Subscriber sub_gps = nh.subscribe<TCPIP::object_msg_arr> ("/gps_msg", 100, recv_gps);
+    ros::Subscriber sub_fusion  = nh.subscribe<TCPIP::object_msg_arr>       ("/fusion_obj", 1, recv_fusion);
+    ros::Subscriber sub_gps     = nh.subscribe<sensor_msgs::NavSatFix>      ("/ublox/fix",  1, recv_gps);
+    ros::Subscriber sub_ins     = nh.subscribe<std_msgs::Float32MultiArray> ("/INS",        1, recv_ins);
 
-    ros::Rate rate(20.);
+    //only send
+    // ros::Rate rate(20.);
+    // while (ros::ok()){
+    //     ros::spinOnce();
+    //     rate.sleep();
+    //     send(sock.second);
+    // }
+
+    //send and receive
     while (ros::ok()){
         ros::spinOnce();
-        rate.sleep();
         send(sock.second);
+        recv(sock.second);
     }
-
-
-
 
     return 0;
 }
